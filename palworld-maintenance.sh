7a3d29f35dc33ad521fedc533b47503a3fd34e8a
Turler <minhdaochi504@gmail.com>
2024-01-26 22:11:20 +0700

    update script

--- palworld-maintenance.sh
+++ palworld-maintenance.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-rm -rf /home/steam/Palworld_backups/Saved && cp -rf /home/flamintdao/Steam/steamapps/common/PalServer/Pal/Saved /home/flamintdao/Palworld_backups/
+rm -rf /home/steam/Palworld_backups/Saved && cp -rf /home/steam/Steam/steamapps/common/PalServer/Pal/Saved /home/steam/Palworld_backups/
