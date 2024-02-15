7afee7b53701405987b5cb04ab1a03455d476ff2
Turler <minhdaochi504@gmail.com>
2024-02-16 00:57:36 +0700

    add entrypoint

---
+++ entrypoint.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+/home/steam/Steam/steamcmd.sh +login anonymous +app_update 1007 +quit
+wait
+/home/steam/Steam/steamapps/common/PalServer/PalServer.sh -useperfthreads -NoAsyncLoadingThread -UseMultithreadForDS
