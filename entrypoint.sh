10955ccb47c2b4b5feaaae261a352fe696a40b84
Turler <minhdaochi504@gmail.com>
2024-02-20 23:39:07 +0700

    remove avoid crash

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 /home/steam/Steam/steamcmd.sh +login anonymous +app_update 1007 +quit
 wait
-/home/steam/Steam/steamapps/common/PalServer/PalServer.sh -NoAsyncLoadingThread -UseMultithreadForDS
+/home/steam/Steam/steamapps/common/PalServer/PalServer.sh
