4733de01a4dc3533c646a31e10c992da921ccd7b
Turler <minhdaochi504@gmail.com>
2024-02-21 14:42:42 +0700

    test param -useperfthreads

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 /home/steam/Steam/steamcmd.sh +login anonymous +app_update 2394010 validate +quit
 wait
-/home/steam/Steam/steamapps/common/PalServer/PalServer.sh -NoAsyncLoadingThread -UseMultithreadForDS
+/home/steam/Steam/steamapps/common/PalServer/PalServer.sh -useperfthreads -NoAsyncLoadingThread -UseMultithreadForDS
