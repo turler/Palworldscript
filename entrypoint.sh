e7b891ccfd8dd9362f89d3359ac356dabb8b1be7
Turler <minhdaochi504@gmail.com>
2024-02-21 17:50:26 +0700

    update

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,8 @@
-#!/bin/bash
+#!/bin/sh
+cron && tail -f /var/log/cron.log
 /home/steam/Steam/steamcmd.sh +login anonymous +app_update 2394010 validate +quit
 wait
-cd /home/steam/Palworld_backups && git pull
+cd /home/steam/Palworld_backups && /usr/bin/git pull
 wait
 rm -rf /home/steam/Steam/steamapps/common/PalServer/Pal/Saved && cp -rf /home/steam/Palworld_backups/Saved /home/steam/Steam/steamapps/common/PalServer/Pal/
 /home/steam/Steam/steamapps/common/PalServer/PalServer.sh -useperfthreads -NoAsyncLoadingThread -UseMultithreadForDS
