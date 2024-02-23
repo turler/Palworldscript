f08cd2aff70280bb94795aca32dcea13c0f876b6
flamint <minhdaochi504@gmail.com>
2024-02-23 17:28:13 +0700

    fix bug

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,4 @@ sudo service cron start
 wait
 /home/steam/Steam/steamcmd.sh +login anonymous +app_update 2394010 validate +quit
 wait
-cd /home/steam/Palworld_backups && /usr/bin/git pull
-wait
-rm -rf /home/steam/Steam/steamapps/common/PalServer/Pal/Saved && cp -rf /home/steam/Palworld_backups/Saved /home/steam/Steam/steamapps/common/PalServer/Pal/
 /home/steam/Steam/steamapps/common/PalServer/PalServer.sh -useperfthreads -NoAsyncLoadingThread -UseMultithreadForDS
