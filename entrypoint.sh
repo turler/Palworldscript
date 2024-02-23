9f6529990c6bda3cf462a1d52c8d570c3c62fc4a
flamint <minhdaochi504@gmail.com>
2024-02-23 18:11:02 +0700

    add comment

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,4 +3,5 @@ sudo service cron start
 wait
 /home/steam/Steam/steamcmd.sh +login anonymous +app_update 2394010 validate +quit
 wait
+# rm -rf /home/steam/Steam/steamapps/common/PalServer/Pal/Saved && cp -rf /home/steam/Palworld_backups/Saved /home/steam/Steam/steamapps/common/PalServer/Pal/
 /home/steam/Steam/steamapps/common/PalServer/PalServer.sh -useperfthreads -NoAsyncLoadingThread -UseMultithreadForDS
