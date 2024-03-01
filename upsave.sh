8617551f601f18e211f36f57e1ccd03924b0cdd5
flamint <minhdaochi504@gmail.com>
2024-03-01 15:56:55 +0700

    update

--- upsave.sh
+++ upsave.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
 rm -rf /home/steam/Palworld_backups
-cd /home/steam/ && /usr/bin/git clone --filter=tree:0 https://github.com/turler/Palworldscript.git
-wait
-rm -rf /home/steam/Steam/steamapps/common/PalServer/Pal/Saved && cp -rf /home/steam/Palworld_backups/Saved /home/steam/Steam/steamapps/common/PalServer/Pal/
+cd /home/steam/ && /usr/bin/git clone --filter=tree:0 git@github.com:turler/Palbackup.git
